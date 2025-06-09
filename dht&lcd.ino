98f0f8fa88054e08991226203c4b1603bac01e52
S-Vinay686 <vv9435912@gmail.com>
2025-06-09 23:30:32 +0530

    Update dht&lcd.ino
    Updated dht form the new manual [only serial moniter]

--- dht&lcd.ino
+++ dht&lcd.ino
@@ -1,60 +1,18 @@
-// ORIGINAL CODE FROM THE BOOK
-#define DHTPIN 2
-#define DHTTYPE DHT11
-DHT11 dht11(2);
-LiquidCrystal lcd(12,11,10,9,8,7);
-void setup()
-{
-lcd.begin(16, 2);
-lcd.clear();
-Serial.begin(9600);
-}
-void loop()
-{
-float humidity = dht11.readHumidity();
-float temperature = dht11.readTemperature();
-lcd.setCursor(0,0);
-lcd.print("RH:");
-lcd.print(humidity);
-lcd.print("%");
-float tempC1=dht11.readTemperature();
-lcd.setCursor(0,1);
-lcd.print("Temp:");
-lcd.print(temperature);
-lcd.print((char)223); //used to display degree symbol on display lcd.print("C\n");
-Serial.print("Realtive Humidity %:");Serial.println(humidity);
-Serial.print("Temperature in degree celcius:");Serial.println(temperature);
-delay(400);
-}
-
-// UPDATED CODE WITHOUT ERROR USING GPT
-#include <Adafruit_Sensor.h>
 #include <DHT.h>
-#include <LiquidCrystal.h>
-#define DHTPIN 2
-#define DHTTYPE DHT11
+#define DHTPIN 2 // DHT11 data pin connected to D2
+#define DHTTYPE DHT11 // Define sensor type
 DHT dht(DHTPIN, DHTTYPE);
-LiquidCrystal lcd(12,11,10,9,8,7);
-void setup()
-{
-lcd.begin(16, 2);
-lcd.clear();
+void setup() {
 Serial.begin(9600);
+dht.begin();
 }
-void loop()
-{
-float humidity = dht.readHumidity();
+void loop() {
 float temperature = dht.readTemperature();
-lcd.setCursor(0,0);
-lcd.print("RH:");
-lcd.print(humidity);
-lcd.print("%");
-float tempC1=dht.readTemperature();
-lcd.setCursor(0,1);
-lcd.print("Temp:");
-lcd.print(temperature);
-lcd.print((char)223); //used to display degree symbol on display lcd.print("C\n");
-Serial.print("Realtive Humidity %:");Serial.println(humidity);
-Serial.print("Temperature in degree celcius:");Serial.println(temperature);
-delay(400);
+float humidity = dht.readHumidity();
+Serial.print("Temp: ");
+Serial.print(temperature);
+Serial.print(" °C, Humidity: ");
+Serial.print(humidity);
+Serial.println(" %");
+delay(2000);
 }
